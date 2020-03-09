d92d81038617996e6d0b8c6c8745c065489912b6
Markshawn <33591398+ShawnMark@users.noreply.github.com>
2020-03-10 05:40:33 +0800

    rm licence

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="myimage", # Replace with your own username
-    version="0.0.3",
+    version="0.0.4",
     author="MarkShawn",
     author_email="shawninjuly@gmail.com",
     description="A handful module for uploading local image and then get the path from specific server, "
